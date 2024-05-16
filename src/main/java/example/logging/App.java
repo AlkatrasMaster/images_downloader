551f72b466539c3632ed8c65a6bdcb145b98f011
Renat Mavlayviev <AlkatrasMaster@yandex.ru>
2024-05-17 01:29:06 +0500

    Урок 19.9 Изучен

--- src/main/java/example/logging/App.java
+++ src/main/java/example/logging/App.java
@@ -1,13 +1,38 @@
 package example.logging;
 
-/**
- * Hello world!
- *
- */
+import example.logging.dowloader.ImageDownloader;
+import example.logging.dowloader.WebScraper;
+
+import java.util.Set;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       String url = "https://skillbox.ru/";
+
+       //относительный путь от корня проекта
+        String dirPath = "./images";
+
+        WebScraper webScraper = new WebScraper(url);
+        Set<String> links = webScraper.getImageLinks();
+
+        ImageDownloader imageDownloader = new ImageDownloader(dirPath);
+
+        for (String link : links) {
+            download(imageDownloader, link);
+        }
+    }
+
+
+    private static void download(ImageDownloader imageDownloader, String link) {
+        try {
+            String downloadedFilePath = imageDownloader.download(link);
+            System.out.println("Изображение по ссылке " + link + " успешно скачано в файл " + downloadedFilePath);
+        } catch (RuntimeException e) {
+            System.err.println("Ошибка скачивания файла" + link + " в директорию" + imageDownloader.getDirPath());
+            e.printStackTrace();
+        }
     }
 }
